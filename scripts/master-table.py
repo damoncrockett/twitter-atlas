4fb05f58e9c7ba61f1a8310d5f74adc085b2f8be
damoncrockett <damoncrockett@hotmail.com>
2017-07-07 13:56:13 -0500

    spaces
    Signed-off-by: damoncrockett <damoncrockett@hotmail.com>

--- scripts/master-table.py
+++ scripts/master-table.py
@@ -17,37 +17,37 @@ measures = ['tweet','actor']
 
 cols = []
 for measure in measures:
-	for year in years:
-		for month in months:
-			cols.append(measure+str(year)+str(month))
+    for year in years:
+        for month in months:
+            cols.append(measure+str(year)+str(month))
 
 cols = cols[:43] # bc we only have into July 2014
 X = pd.DataFrame(index=cities,columns=cols)
 
 for city in X.index:
-	for year in years:
-		
+    for year in years:
+        
         # hack bc I'm dumb
-		if year==2014:
-			yearstring = ''
-		else:
-			yearstring = str(year)
+        if year==2014:
+            yearstring = ''
+        else:
+            yearstring = str(year)
 
-		tablestr = TABLEDIR + city + "_" + yearstring + ".csv"
-		table = pd.read_csv(tablestr)
+        tablestr = TABLEDIR + city + "_" + yearstring + ".csv"
+        table = pd.read_csv(tablestr)
 
-		table.postedTime = table.postedTime.apply(parser.parse)
-		table['month'] = [item.month for item in table.postedTime]
+        table.postedTime = table.postedTime.apply(parser.parse)
+        table['month'] = [item.month for item in table.postedTime]
 
-		for month in months:
-			tmp = table[table.month==month]
+        for month in months:
+            tmp = table[table.month==month]
 
-			for measure in measures:
+            for measure in measures:
                 
                 if measure=='tweet':
                     X.loc[city,measure+str(year)+str(month)] = len(tmp)
                 elif measure=='actor':
-                	X.loc[city,measure+str(year)+str(month)] = len(tmp.actor_id.unique())
+                    X.loc[city,measure+str(year)+str(month)] = len(tmp.actor_id.unique())
 
 df = df.join(X,on="Center")
 df.to_csv(DIR+"master-table-500.csv",index=False)
