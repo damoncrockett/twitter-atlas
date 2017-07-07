cd3845847712da264213f30b9db3a2be52dfd70e
damoncrockett <damoncrockett@hotmail.com>
2017-07-06 20:38:40 -0500

    500
    Signed-off-by: damoncrockett <damoncrockett@hotmail.com>

---
+++ scripts/city-bound-500.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import numpy as np
+
+PRE = "/data/damoncrockett/"
+DIR = PRE + "twitter-atlas/"
+TWEETDIR = DIR + "tweets/"
+TARGET = DIR + "500-city-tables-10km/"
+DATA = pd.read_csv(DIR + "world_ua_bboxes.csv")
+
+tweettables = [
+    "twitter_metadata_2011.csv",
+    "twitter_metadata_2012.csv",
+    "twitter_metadata_2013.csv",
+    "twitter_metadata_2014_mezzo.csv",
+]
+
+# 1 degree latitude = 110.574 km
+# 1 degree longitude = 111.320 * cos(latitude) km
+# And make sure we use radians when computing cosine!
+
+lat_margin = 5 / 110.574 #bc we want 10km box
+def lon_margin(lat):
+    margin = 5 / ( 111.320 * np.cos( np.radians(lat) ) )
+    return margin
+
+for tweettable in tweettables:
+    year = tweettable.split("_")[2][:-4]
+
+    df = pd.read_csv(PRE + tweettable)
+    df = df[['mongo_id','lat','lon','postedTime']]
+
+    for i in DATA.index:
+
+        city = DATA.Center.loc[i]
+        lon = DATA.lon.loc[i]
+        lat = DATA.lat.loc[i]
+
+        lat_min = lat - lat_margin
+        lat_max = lat + lat_margin
+        lon_min = lon - lon_margin(lat)
+        lon_max = lon + lon_margin(lat)
+
+        tmp = df[df.lat > lat_min]
+        tmp = tmp[tmp.lat < lat_max]
+        tmp = tmp[tmp.lon > lon_min]
+        tmp = tmp[tmp.lon < lon_max]
+
+        print(year,i,city)
+
+        tmp.to_csv(TARGET+city+"_"+year+".csv",index=False)
+        
--- svgplots/scripts/rhythm.R
+++ svgplots/scripts/rhythm.R
@@ -5,13 +5,50 @@ df = read.csv("/Users/damoncrockett/twitter-atlas/tables/pop-vol-div-geo-sorted-
 
 library(ggplot2)
 
+df <- df[order(df$tweets),]
+
 twelve$label = factor(twelve$label,levels=as.vector(df$label))
 fourteen$label = factor(fourteen$label,levels=as.vector(df$label))
 
 ggplot(twelve,aes(secpast)) + 
-  geom_line(stat="density",linetype="dotted",color="grey25") +
-  geom_line(data=fourteen,stat="density",color="grey25") +
+  geom_line(stat="density",
+            color="grey25",
+            adjust=1) +
+  geom_line(data=fourteen,
+            stat="density",
+            color="grey25",
+            linetype="dotted",
+            adjust=1) +
   coord_polar() +
+  facet_wrap(~label) +
+  theme(
+    text = element_text(family = "mono", 
+                        face = "plain",
+                        size = 28), 
+    legend.position = "none",
+    axis.text = element_blank(),
+    axis.ticks = element_blank(),
+    panel.background = element_blank(), 
+    panel.border = element_blank(), 
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    axis.title.y = element_blank(),
+    strip.background = element_blank(),
+    strip.text = element_text(color="grey25")
+  ) +
+  labs(x="",y="")
+
+c = df$label[33:81]
+
+twelve.t = twelve[twelve$label %in% c,]
+fourteen.t = fourteen[fourteen$label %in% c,]
+
+ggplot(twelve.t,aes(secpast,..density..)) + 
+  geom_histogram(bins=168,fill="blue",alpha=0.5) +
+  geom_histogram(data=fourteen.t,bins=168,fill="orange",alpha=0.5) +
+  #geom_density(bw=10000,color="blue") +
+  #geom_density(data=fourteen.t,bw=10000,color="orange") +
+  #coord_polar() +
   facet_wrap(~label) +
   theme(
     text = element_text(family = "mono", 
