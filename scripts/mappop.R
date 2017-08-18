f344827ea59f6a70cb0345bf587637d0fdcd86f0
damoncrockett <damoncrockett@hotmail.com>
2017-08-18 16:33:13 -0500

    plots
    Signed-off-by: damoncrockett <damoncrockett@hotmail.com>

---
+++ scripts/mappop.R
@@ -0,0 +1,19 @@
+library(ggplot2)
+df = read.csv("~/Desktop/100cities_subcontinents.csv")
+
+df$totaltweets = rowSums(df[,c(5:39)])
+  
+ggplot(df,aes(lon,lat,size=totaltweets)) +
+  borders(database = "world",
+          colour="grey45",
+          fill="grey25") +
+  theme(panel.background = element_rect(fill="grey65"),
+        panel.grid.major = element_line(color="grey65"),
+        panel.grid.minor = element_line(color="grey65"),
+        plot.background = element_rect(fill="grey65"),
+        legend.position="none",
+        axis.text = element_blank(),
+        text = element_blank(),
+        axis.ticks = element_blank()) +
+  geom_point(color = "#1b79ff",alpha=0.5) +
+  scale_size_continuous(range=c(1,2000))
