debea0bc0dcba3eff9b684a860397183e8161c4d
damoncrockett <damoncrockett@hotmail.com>
2017-08-19 14:42:57 -0500

    single
    Signed-off-by: damoncrockett <damoncrockett@hotmail.com>

--- scripts/weeks_single.R
+++ scripts/weeks_single.R
@@ -14,7 +14,8 @@ for (i in c(149:0)) {
         theme(panel.background = element_rect(fill="grey65"),
               panel.grid.major = element_line(color="grey65"),
               panel.grid.minor = element_line(color="grey65"),
-              plot.background = element_rect(fill="grey65"),
+              panel.border = element_blank(),
+              plot.background = element_rect(fill="grey65",color="grey65"),
               legend.position="none",
               axis.text = element_blank(),
               text = element_blank(),
@@ -22,8 +23,8 @@ for (i in c(149:0)) {
         geom_point(color = "#1b79ff",
                    size = 0.1)
     ggsave(paste0(targetpath,i,".png"),
-           width = 64,
-           height = 36,
+           width = 128,
+           height = 72,
            units = "in",
            limitsize = 0)
     print(i)
