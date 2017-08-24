bf9fe2bb6667ff021763d153097c5aef24abf88a
damoncrockett <damoncrockett@hotmail.com>
2017-08-24 04:18:16 -0500

    mo maps
    Signed-off-by: damoncrockett <damoncrockett@hotmail.com>

--- scripts/city-maps.R
+++ scripts/city-maps.R
@@ -1,5 +1,7 @@
 library(ggmap)
+setwd("~/Desktop/")
 df = read.csv("London_geo.csv")
+
 
 left = min(df$lon)
 right = max(df$lon)
@@ -8,26 +10,31 @@ bottom = min(df$lat)
 
 location = c(left,bottom,right,top)
 
+locdf = data.frame(lon = c(left,right),
+                   lat = c(bottom,top))
+
 basemap = ggmap(get_map(location=location,
                        source = "stamen",
-                       maptype = "toner-lite"))
+                       maptype = "toner-background"))
 
-basemap + geom_point(data=df,
+basemap + geom_point(data=locdf,
                     aes(lon,lat),
                     color="#1b79ff",
-                    size=0.1,
-                    shape=1) +
+                    size=10,
+                    alpha = 0.5) +
   theme(text=element_blank(),
         axis.text=element_blank(),
         axis.ticks=element_blank())
 
-qmplot(lon,lat,data=df,
+
+
+dfs = df[sample(nrow(df), 1000000), ]
+qmplot(lon,lat,data=dfs,
        maptype="toner-background",
        darken=0.7,
-       color="#1b79ff",
-       size=0.1) 
+       size = I(0.1),
+       color = I("#1b79ff")) 
   
-
 ggsave("zoom-london.png",
        width=72,
        height=72,
