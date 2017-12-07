e4cead55beb5bffa9710e004c49de2458d8065ee
damoncrockett <damoncrockett@hotmail.com>
2017-12-07 14:23:39 -0500

    hashtag-tables
    Signed-off-by: damoncrockett <damoncrockett@hotmail.com>

--- scripts/hashtag-tables.py
+++ scripts/hashtag-tables.py
@@ -12,12 +12,14 @@ def hashparse(item):
     item = item.lstrip("[").rstrip("]")
     l = item.split(",")
     l = [item.lstrip(" ").rstrip(" ") for item in l]
+    l = '_'.join(l)
     return l
     
 counter = -1    
 for hashtag in hashtable.hashtag:
     counter+=1
-    
+    hashstring = "_"+hashtag+"_" # such a hack here...
+
     try:
         os.mkdir(HASH+hashtag+"/")
     except:
@@ -28,24 +30,15 @@ for hashtag in hashtable.hashtag:
         tcounter+=1
         basename = os.path.basename(tweettable)
         df = pd.read_csv(tweettable)
+        df = df[df.hashtags!='[]']
         df.hashtags = df.hashtags.apply(hashparse)
+        df = df[df.hashtags.str.contains(hashstring)]
         
-        taginlist = []
-        for i in df.index:
-            hashtaglist = df.hashtags.loc[i]
-            if hashtag in hashtaglist:
-                taginlist.append('yes')
-            else:
-                taginlist.append('no')
-            print(counter,hashtag,tcounter,basename,i)
-
-        df['taginlist'] = taginlist
-        df = df[df.taginlist=='yes']
-
         if tcounter==0:
             tmp = df
         else:
             tmp = tmp.append(df)
+
+        print(counter,hashtag,tcounter,basename)
     
-    del tmp['taginlist']
     tmp.to_csv(HASH+hashtag+".csv",index=False)
